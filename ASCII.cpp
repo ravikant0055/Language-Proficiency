d1db1fafba04af838270b11d09e1259421198de0
ravikant0055 <kumarra927@gmail.com>
2021-04-30 21:58:53 +0530

    commit

---
+++ ASCII.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    char n ;
+
+    cout<<"Enter a charactar : ";
+    cin>>n;
+    cout<<"ASCII value of "<<n<<" is : "<<int(n);
+    
+    return 0;
+}

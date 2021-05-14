e248625e71e43c8e563ad76a2f1c01545214d422
ravikant0055 <kumarra927@gmail.com>
2021-05-14 21:30:44 +0530

    commit

---
+++ SumofAllSubarray.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n, sum=0;
+    cout<<"Enter the num of element in array : ";
+    cin>>n;
+    
+    int arr[n];
+    cout<<"Enter the value of array"<<endl;
+    for(int i=0; i<n; i++)
+    {
+        cin>>arr[i];
+    }
+    
+    cout<<"Sum of subarray"<<endl;
+    for(int i=0; i<n; i++)
+    {
+        for(int j=i; j<n; j++)
+        {
+            sum+=arr[j];
+            cout<<sum<<endl;
+        }
+    }
+
+     return 0;
+
+}
